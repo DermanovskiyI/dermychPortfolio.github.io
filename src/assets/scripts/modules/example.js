2a5a1b78df8cab6158bf1dac3f072a93cef78dda
Igor Dermanovsky <igorderman@gmail.com>
2021-02-11 12:05:51 +0300

    third section and start create footer

---
+++ src/assets/scripts/modules/example.js
@@ -0,0 +1,46 @@
+
+ymaps.ready(init);
+
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [55.99025818, 37.21285228],
+            zoom: 12,
+            controls: ['zoomControl', 'searchControl']
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+    // Создаем геообъект с типом геометрии "Точка".
+        myGeoObject = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [55.99025818, 37.21285228]
+            },
+            
+        }, {
+            // Опции.
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'islands#blackStretchyIcon',
+            // Метку можно перемещать.
+            draggable: true
+        });
+       
+
+    myMap.geoObjects
+
+        .add(new ymaps.Placemark([55.99025818, 37.21285228], {
+            balloonContent: 'ЗДАРОВА',
+            iconCaption: 'АГА'
+        }, {
+            preset: 'islands#blueCircleDotIconWithCaption',
+            iconCaptionMaxWidth: '500'
+        }));
+
+
+        myMap.behaviors
+        // Отключаем выключенное поведение:
+        //  - scrollZoom - зум по скроллу
+        .disable(['scrollZoom'])
+}
