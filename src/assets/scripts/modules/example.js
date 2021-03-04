064b2cdeecc2c1664d031e5756fa92da86d143c3
Igor Dermanovsky <igorderman@gmail.com>
2021-03-05 00:32:49 +0300

    vse pravki vneseni

--- src/assets/scripts/modules/example.js
+++ src/assets/scripts/modules/example.js
@@ -6,9 +6,9 @@ ymaps.ready(init);
 
 function init() {
     var myMap = new ymaps.Map("map", {
-            center: [55.99025818, 37.21285228],
+            center: [55.980951, 37.130937],
             zoom: 12,
-            controls: ['zoomControl']
+            controls: []
         }, {
             searchControlProvider: 'yandex#search'
         }),
@@ -50,18 +50,18 @@ function init() {
 
 //// HAMBURGER
 
-const heroMenu = document.querySelector('.hero__menu');
-const hamburger = document.querySelector('.hamburger__menu');
-const popup = document.querySelector('.popup');
+const hamburger = document.querySelector('.hamburger');
+const hamburgerMenu = document.querySelector('.hamburger__menu');
+const popup = document.getElementById('popup');
 const body = document.querySelector('body');
 
-heroMenu.addEventListener('click', function(e) {
-    if(e.target.classList.contains('hamburger__menu') || e.target.classList.contains('hero__menu')) {
-        hamburger.classList.toggle('hamburger__menu--animated');
+hamburger.addEventListener('click', function(e) {
+    if(e.target.classList.contains('hamburger__menu') || e.target.classList.contains('hamburger')) {
+        hamburgerMenu.classList.toggle('hamburger__menu--animated');
         popup.classList.toggle('popup--active');
         body.classList.toggle('body--without-scroll');
     }
-    
+
 })
 
 
