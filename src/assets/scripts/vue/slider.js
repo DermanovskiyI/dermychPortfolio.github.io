ff7707dac188766536055db2d1d2cc0b42af425b
Igor Dermanovsky <igorderman@gmail.com>
2021-06-16 03:01:57 +0300

    get works in mpa from server

--- src/assets/scripts/vue/slider.js
+++ src/assets/scripts/vue/slider.js
@@ -1,124 +1,126 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import axios from 'axios';
 
 const sliderButtons = {
 
-    methods: {
-        slide(direction) {
-            this.$emit('slide', direction)
-        },
-        btnSlide(direction) {
-            this.$emit('btnSlide', direction)
-        }
-    },
-    props: {
-        work: Object,
-        nextIndex: Number,
-        prevIndex: Number,
-        nextWork: Object,
-        prevWork: Object,
-    },
-    template: '#slider-buttons',
+  methods: {
+      slide(direction) {
+          this.$emit('slide', direction)
+      },
+      btnSlide(direction) {
+          this.$emit('btnSlide', direction)
+      }
+  },
+  props: {
+      work: Object,
+      nextWork: Object,
+      prevWork: Object,
+  },
+  template: '#slider-buttons',
 }
 const sliderDisplay = {
-    props: {
-        work: Object
-    },
-    template: '#slider-display',
+  props: {
+      work: Object
+  },
+  template: '#slider-display',
 
 }
 const sliderInfo = {
-    props: {
-        work: Object
-    },
-    template: '#slider-info',
+  props: {
+      work: Object
+  },
+  template: '#slider-info',
 }
 
 new Vue({
-    el: '#vue-slider',
-    components: {
-        sliderInfo,
-        sliderDisplay,
-        sliderButtons
-    },
-    data() {
-        return {
-            works: [],
-            currentIndex: 0,
-            nextIndex: 1,
-            prevIndex: 0
-        }
-    },
-    computed: {
-        work() {
-            return this.works[this.currentIndex]
-        },
-        nextWork() {
-            return this.works[this.nextIndex]
-        },
-        prevWork() {
-            return this.works[this.prevIndex]
-        },
+  el: '#vue-slider',
+  components: {
+      sliderInfo,
+      sliderDisplay,
+      sliderButtons
+  },
+  data() {
+      return {
+          works: [],
+          currentIndex: 0,
+          nextIndex: 1,
+          prevIndex: 0
+      }
+  },
+  computed: {
+      work() {
+          return this.works[this.currentIndex]
+      },
+      nextWork() {
+          return this.works[this.nextIndex]
+      },
+      prevWork() {
+          return this.works[this.prevIndex]
+      },
 
-    },
-    created() {
-        this.works = require('../../../data/works.json');
-        this.prevIndex = this.works.length - 1
-    },
-    methods: {
-        changeWork(direction) {
-            const firstIndex = 0;
-            const lastIndex = this.works.length - 1;
+  },
+  created() {
+      axios.get('https://webdev-api.loftschool.com/works/439').then(response => {
+          this.works = response.data;
+          this.prevIndex = this.works.length - 1;
+      }).catch(error => {
+          console.log(error);
+      })
+      
+  },
+  methods: {
+      changeWork(direction) {
+          const firstIndex = 0;
+          const lastIndex = this.works.length - 1;
 
-            if (direction == 'next') {
+          if (direction == 'next') {
 
-                if (this.currentIndex < lastIndex) {
-                    this.currentIndex++;
-                } else {
-                    this.currentIndex = firstIndex;
-                }
+              if (this.currentIndex < lastIndex) {
+                  this.currentIndex++;
+              } else {
+                  this.currentIndex = firstIndex;
+              }
 
-            } else {
-                if (this.currentIndex > firstIndex) {
-                    this.currentIndex--;
-                } else {
-                    this.currentIndex = lastIndex;
-                }
-            }
-        },
-        btnSlide(direction) {
-            const firstIndex = 0;
-            const lastIndex = this.works.length - 1;
-            if (direction == 'next') {
-                if (this.nextIndex < lastIndex) {
-                    this.nextIndex++;
+          } else {
+              if (this.currentIndex > firstIndex) {
+                  this.currentIndex--;
+              } else {
+                  this.currentIndex = lastIndex;
+              }
+          }
+      },
+      btnSlide(direction) {
+          const firstIndex = 0;
+          const lastIndex = this.works.length - 1;
+          if (direction == 'next') {
+              if (this.nextIndex < lastIndex) {
+                  this.nextIndex++;
 
-                } else {
-                    this.nextIndex = firstIndex
-                }
+              } else {
+                  this.nextIndex = firstIndex
+              }
 
-                if (this.prevIndex == lastIndex) {
-                    this.prevIndex = firstIndex
-                } else {
-                    this.prevIndex++
-                }
+              if (this.prevIndex == lastIndex) {
+                  this.prevIndex = firstIndex
+              } else {
+                  this.prevIndex++
+              }
 
-            } else {
-                if (this.nextIndex == firstIndex) {
-                    this.nextIndex = lastIndex;
-                } else {
-                    this.nextIndex--
-                }
+          } else {
+              if (this.nextIndex == firstIndex) {
+                  this.nextIndex = lastIndex;
+              } else {
+                  this.nextIndex--
+              }
 
-                if (this.prevIndex == firstIndex) {
-                    this.prevIndex = lastIndex
-                } else {
-                    this.prevIndex--
-                }
-            }
+              if (this.prevIndex == firstIndex) {
+                  this.prevIndex = lastIndex
+              } else {
+                  this.prevIndex--
+              }
+          }
 
-        }
-    },
-    template: '#slider-container'
-
-
+      }
+  },
+  template: '#slider-container'
 })
