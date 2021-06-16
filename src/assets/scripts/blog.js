58685b5e0e99d1244d04f2752fc6d60aba9f5d86
Igor Dermanovsky <igorderman@gmail.com>
2021-06-17 01:14:09 +0300

    resolved error in blog with hamburger

--- src/assets/scripts/blog.js
+++ src/assets/scripts/blog.js
@@ -1,1 +1,2 @@
 import "./modules/scrollDown";
+import "./modules/hamburger";
--- src/assets/scripts/modules/scrollDown.js
+++ src/assets/scripts/modules/scrollDown.js
@@ -4,27 +4,8 @@ const arrowDown = document.querySelector('.scroll-down')
 arrowDown.addEventListener('click', (e)=> {
     e.preventDefault();
     window.scrollTo({
-        top:1000,
+        top: window.innerHeight,
         behavior: "smooth"
     });
 })
 
-
-// const scrollBtn = document.getElementById('scroll-btn');
-
-// scrollBtn.addEventListener('click', () => {
-//     window.scrollTo({
-//         top: 0,
-//         behavior: "smooth"
-//     });
-// })
-// document.addEventListener('scroll', () => {
-//     const currentCoords = window.pageYOffset;
-//     const coordWhileShowBtn = 100;
-//     if (currentCoords > coordWhileShowBtn) {
-//         scrollBtn.classList.remove('scroll--hidden');
-//     } else {
-//         scrollBtn.classList.add('scroll--hidden');
-//     }
-
-// })
